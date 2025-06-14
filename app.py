7e92e9a9e2097d602c81438650f63bd13e4c5043
joacolara5 <joacomlara@gmail.com>
2025-06-14 09:20:18 -0400

    app

--- app.py
+++ app.py
@@ -759,4 +759,6 @@ if __name__ == '__main__':
     with app.app_context():
         sync_sequences()
     app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render asigna el puerto dinámicamente
+    app.run(host='0.0.0.0', port=port)
     
