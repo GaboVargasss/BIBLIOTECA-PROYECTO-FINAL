34fafc6463e71fe9b501251503d9e42f42f73085
joacolara5 <joacomlara@gmail.com>
2025-06-14 09:31:04 -0400

    Update app.py

--- app.py
+++ app.py
@@ -758,8 +758,8 @@ def download_report(report_type):
 if __name__ == '__main__':
     with app.app_context():
         sync_sequences()
-    app.run(debug=True)
+    ##app.run(debug=True)
     port = int(os.environ.get("PORT", 5000))  # Render asigna el puerto dinámicamente
     app.run(host='0.0.0.0', port=port)
-    ##hola
+    
     
